318121266aa3683e336f36f31d0deee3f879c9ee
Prabal Paliwal <paliwalprabal.gps@gmail.com>
2026-09-02 19:00:02 -0400

    Finish Footer and contact information

--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -2,12 +2,42 @@ import Hero from "./hero.tsx"
 import Squiggle from "./squiggle.tsx"
 import Nav from "./Nav.tsx"
 import "./style.css"
+import Heading from "./heading.tsx"
+import Footer from "./footer.tsx"
 
 function Contacts(){
     return <>
         <Nav></Nav>
         <Hero title="/title_contacts.png" background_image="linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('/school_background.png')"></Hero>
         <Squiggle></Squiggle>
+        <div className="containers">
+        <Heading word="Contact Information"></Heading>
+        <div className="contact-container">
+            <div className="contact-card light">
+                <h3>Email</h3>
+                <div><img className="contact-logo" src="/email.png"></img><a>hyperlinked</a></div>
+                <div><img className="contact-logo" src="/email.png"></img><a>hyperlinked</a></div>
+                <div><img className="contact-logo" src="/email.png"></img><a>hyperlinked</a></div>
+            </div>
+            <div style={{display:'grid', gridTemplateColumns:'1fr'}}>
+                <div className="contact-card light">
+                    <h3>Instagram</h3>
+                    <div><img className="contact-logo" src="/insta.png"></img><a href='https://www.instagram.com/stuyjuniorcaucus/' target="_black">hyperlinked</a></div>
+                </div>
+                <div className="contact-card light">
+                    <h3>Linktree</h3>
+                    <div><img className="contact-logo" src="/linktree.png"></img><a href="https://linktr.ee/juniorcaucus2627?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAcGRvZgJleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA85MzY2MTk3NDMzOTI0NTkAAad7fJuZtVlHOvSobeWzInc1NUdzmz7T0ndstx9toMF_9-FUpfBB1dTFsCN_nw_aem_pWjyy-acopt8XzoK1oB9jA" target="_black">hyperlinked</a></div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <Squiggle></Squiggle>
+        <div className="containers checker">
+        <Heading word='Suggestions'></Heading>
+        
+        </div>
+        <Squiggle></Squiggle>
+        <Footer></Footer>
     </>;
 }
 
--- src/footer.tsx
+++ src/footer.tsx
@@ -1,7 +1,23 @@
+import { Square } from "lucide-react"
+import Heading from "./heading"
+import Squiggle from "./squiggle"
+
 function Footer(){
-    return <div className="hero" style={{background: "linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('/school_background.png')", height: '100vh'}}>
-        <p style={{fontWeight: 'bold',color: '#212C37',margin: 'auto', fontSize: '3rem', textAlign:'center'}}>Footer To Be Made</p>
-    </div>
+    return<>
+        <div className="containers">
+        <h2 className="heading footer-title" style={{textAlign:'start', paddingLeft: '20px'}}>Stuyvesant Junior Caucus</h2>
+        <div className="footer-container">
+            <div className="footer-info" style={{marginBottom: '10px',color: '#547fa8', justifySelf:'start', paddingLeft:'20px'}}>
+                Elly Wu Feng and Olivia Liu<br></br>
+                <div style={{display:'grid', gridTemplateColumns:'1fr 1fr'}}>
+                <a style={{width: '81px'}} href='https://www.instagram.com/stuyjuniorcaucus/' target="_black"><img src='/insta.png'></img></a>
+                <a style={{width: '81px'}} href="https://linktr.ee/juniorcaucus2627?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAcGRvZgJleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA85MzY2MTk3NDMzOTI0NTkAAad7fJuZtVlHOvSobeWzInc1NUdzmz7T0ndstx9toMF_9-FUpfBB1dTFsCN_nw_aem_pWjyy-acopt8XzoK1oB9jA" target="_black"><img src='/linktree.png'></img></a>
+                </div>
+            </div>
+            <div style={{color: '#547fa8',justifySelf:'end', paddingRight: '50px'}}><span style={{fontSize: '1.1rem'}}>Developers</span><br></br>IT: Prabal Paliwal<br></br>Design: Chloe Lin, Ivy Li</div>
+        </div>
+        </div>
+        </>
 }
 
 export default Footer
